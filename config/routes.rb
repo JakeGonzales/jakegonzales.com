ed40a353c949a804c776ca9380ec528afd0ba2fd
Jake Gonzales <jake@twoofthree.com>
2018-06-09 16:43:42 +0800

    Setup static pages and CSS pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Static pages 
+  get "/pages/:page" => "pages#show"
+
+  # Homepage
+  root "pages#show", page: "home"
 end
