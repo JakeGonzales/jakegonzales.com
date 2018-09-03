5b5444aa0b935bec22cb91777fdc4e408096e6db
Jake Gonzales <jake@twoofthree.com>
2018-09-03 15:04:55 +0800

    Add skills page and basic content
    Add new routes for static pages
    Add viewport width meta for mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,2 @@
 class PagesController < ApplicationController
-
-    # Source: from http://blog.teamtreehouse.com/static-pages-ruby-rails
-    def show
-      if valid_page?
-        render template: "pages/#{params[:page]}"
-      else
-        render file: "public/404.html", status: :not_found
-      end
-    end
-
-    private
-
-    # Error processing if a page doesn't exist
-    def valid_page?
-      File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
-    end
-  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Static pages 
-  get "/pages/:page" => "pages#show"
-
   # Homepage
-  root "pages#show", page: "home"
+  root :to => 'pages#home'
+
+  # Static Pages
+  get '/skills', to: 'pages#skills', as: 'skills'
+
 end
