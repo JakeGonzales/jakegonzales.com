f60fb4c3a8fdd03ed3ed0eeea2683a3ba3ff2c21
Jake Gonzales <jake@twoofthree.com>
2018-12-19 20:06:54 +0800

    Add static work routes
    Add work views for each project
    Add work controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   # Static Pages
   get '/skills', to: 'pages#skills', as: 'skills'
   get '/work', to: 'pages#work', as: 'work'
-  get '/work/single', to: 'pages#work-single', as: 'work-single'
+
+  # Work Items
+  get '/work/muh-invite-case-study', to: 'work#muh_invite', as: 'muh_invite'
+  get '/work/freo-royale-animation', to: 'work#anomaly', as: 'anomaly'
+  get '/work/bates-saddles', to: 'work#bates', as: 'bates'
+  get '/work/twoofthree-animation', to: 'work#glitched', as: 'glitched'
+  get '/work/izo-photography', to: 'work#izo', as: 'izo'
+  get '/work/star-international', to: 'work#star', as: 'star'
+  get '/work/wa-exclusive-meats', to: 'work#wa_meats', as: 'wa_meats'
+  get '/work/wintec-saddles', to: 'work#wintec', as: 'wintec'
 
 end
